689c0180eb2cbb387d62b50b9bb94641e9b802f4
Vikas Tiwari <vikastiwari809028@gmail.com>
2024-08-28 09:43:43 +0530

    layout Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Hero from "@/components/Hero";
-
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
+import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Portfolio</h1>
-      <Hero></Hero>
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-1 overflow-clip">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid></Grid>
+        <RecentProjects></RecentProjects>
+        <Clients></Clients>
+        <Experience></Experience>
+        <Approach />
+        <Footer></Footer>
+      </div>
+    </main>
   );
 }
