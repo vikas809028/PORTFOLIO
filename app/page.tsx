ef0d714fcb85b516a7eb45c9ff6770d9e22b8975
Vikas Tiwari <vikastiwari809028@gmail.com>
2024-08-28 09:23:46 +0530

    configuration Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-   
+    <div>
+      <h1>Hello Portfolio</h1>
+      <Hero></Hero>
+    </div>
   );
 }
---
+++ data/index.ts
@@ -0,0 +1,154 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I have Learn these skills but constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-3",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+        id: 5,
+        title: "Currently working on a CourseCrafter project",
+        description: "The Inside Scoop",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+      },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "CourseCrafter",
+      des: "Best Online Education platform with experienced teachers at minimalist prize",
+      img: "/CourseCrafter.webp",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/stream.svg"],
+      link: "/ui.earth.com",
+    },
+    {
+      id: 2,
+      title: "TicTacToe",
+      des: "A basic game to get hands on experirence of gaming .",
+      img: "/tictac.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+      link: "/ui.yoom.com",
+    },
+    
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Vikas was an absolute pleasure. His promptness, and dedication to delivering exceptional results were evident throughout our project. Vikas's enthusiasm for every face of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vikas is the ideal partner.",
+      name: "Sarika Agrawal",
+      title: "Director of IOL",
+    },
+    {
+      quote:
+        "Collaborating with Vikas was an absolute pleasure. His promptness, and dedication to delivering exceptional results were evident throughout our project. Vikas's enthusiasm for every face of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vikas is the ideal partner.",
+      name: "Sarika Agrawal",
+      title: "Director of IOL",
+    },
+    {
+      quote:
+        "Collaborating with Vikas was an absolute pleasure. His promptness, and dedication to delivering exceptional results were evident throughout our project. Vikas's enthusiasm for every face of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vikas is the ideal partner.",
+      name: "Sarika Agrawal",
+      title: "Director of IOL",
+    },
+    {
+      quote:
+        "Collaborating with Vikas was an absolute pleasure. His promptness, and dedication to delivering exceptional results were evident throughout our project. Vikas's enthusiasm for every face of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vikas is the ideal partner.",
+      name: "Sarika Agrawal",
+      title: "Director of IOL",
+    },
+    {
+      quote:
+        "Collaborating with Vikas was an absolute pleasure. His promptness, and dedication to delivering exceptional results were evident throughout our project. Vikas's enthusiasm for every face of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vikas is the ideal partner.",
+      name: "Sarika Agrawal",
+      title: "Director of IOL",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Full statck Intern at SoftPro India",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-1",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Campus Ambassdor",
+      desc: "Explored New technologies and hands on experience in team mangement",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    }, 
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
