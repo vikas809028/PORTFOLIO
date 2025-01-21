b8aa1a1375bf957e683d3d2604f75bde820cf959
Vikas Tiwari <vikastiwari809028@gmail.com>
2025-01-21 22:51:49 +0530

    layout fixed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -80,7 +80,6 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
         "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
@@ -110,7 +109,6 @@ export const BentoGridItem = ({
             <img
               src={spareImg}
               alt={spareImg}
-              //   width={220}
               className="object-cover object-center w-full h-full"
             />
           )}
