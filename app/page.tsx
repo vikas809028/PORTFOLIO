d75054e61234e74e9afb04b90d53f9bd52554156
vikas809028 <vikastiwari809028@gmail.com>
2025-02-11 20:58:48 +0530

    lazy loading added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 import RecentProjects from "@/components/RecentProjects";
@@ -7,13 +6,16 @@ import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
 import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
+
+const LazyGrid = dynamic(() => import("@/components/Grid"), { ssr: false });
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-1 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid></Grid>
+        <LazyGrid ></LazyGrid>
         <RecentProjects></RecentProjects>
         <Clients></Clients>
         <Experience></Experience>
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -6,6 +6,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
 import Link from "next/link";
+import Image from "next/image";
 
 const RecentProjects = () => {
   return (
@@ -26,9 +27,10 @@ const RecentProjects = () => {
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
-                  <img src="/bg.png" alt="bgimg" />
+                  <img src="/bg.png" alt="bgimg"  height={200}
+                  width={689} />
                 </div>
-                <img
+                <Image
                   src={item.img}
                   alt="cover"
                   className="z-10 absolute top-30 "
